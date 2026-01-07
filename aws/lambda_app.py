20c3ceece11283a7e6140f8c7c66a4da17e841e7
puresteelco <puresteelco@users.noreply.github.com>
2026-01-07 10:47:35 -0500

    test

--- aws/lambda_app.py
+++ aws/lambda_app.py
@@ -1,6 +1,6 @@
 from s3_data_fetcher import S3DataFetcher
-from Common.exam_data_processor import ExamDataProcessor
-from Common.contracts import read_and_compute
+from common.exam_data_processor import ExamDataProcessor
+from common.contracts import read_and_compute
 
 def build_html(stats):
     return f"""<!DOCTYPE html>
