0446dcef36c890ff9577d8503eb219c544c0eabb
Germs & Spices <123396762+tblst8080@users.noreply.github.com>
2026-01-07 15:12:12 -0500

    cleanup
    modified pytest

--- test_fetch.py
+++ test_fetch.py
@@ -1,30 +1,32 @@
 import pathlib
 import pytest
 import os
+from fetch.api import call_api
 
 def test_csv():
 
     # Check if 'stocks.csv' exists
     if not pathlib.Path("fetch/stocks.csv").exists():
         raise FileNotFoundError("stocks.csv not found.")
-    #
-    # else:
-    #     df = pd.read_csv("fetch/stocks.csv")
-    #
-    #     # Check if 'symbol' column exists
-    #     if 'symbol' not in df.columns:
-    #         raise EmptyHeaderError('Incorrect header.')
-    #
-    #     # Check if 'symbol' column is empty
-    #     if len(df['symbol']) == 0:
-    #         raise EmptyDataError("Empty stock list.")
-    #
-    #     for symbol in df['symbol']:
-    #
-    #         # Attempt api call
-    #         info = finnhub_client.quote(symbol)
-    #
-    #         # Check if call response is valid
-    #         if None in info.values() or 'error' in info.keys():
-    #             raise ValueError(f"Invalid stock symbol: {symbol}")
+
+    else:
+        with pathlib.Path('fetch/stocks.csv').open(mode='r') as file:
+            stocks = file.read().split("\n")
+
+        # Check if 'symbol' column exists
+        if stocks[0] != 'symbol':
+            raise KeyError('Incorrect header.')
+
+        # Check if 'symbol' column is empty
+        if len(stocks) < 2:
+            raise ValueError("Empty stock list.")
+
+        for symbol in stocks[1:]:
+
+            # Attempt api call
+            info = call_api(symbol)
+
+            # Check if call response is valid
+            if None in info.values() or 'error' in info.keys():
+                raise ValueError(f"Invalid stock symbol: {symbol}")
 
