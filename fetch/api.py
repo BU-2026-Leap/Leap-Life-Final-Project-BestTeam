8351981c9703a9f1dd5f30aacedc70a5a4eb1158
Germs & Spices <123396762+tblst8080@users.noreply.github.com>
2026-01-07 12:09:17 -0500

    bug fix
    pytest: format
    fetch\api.py: reference fetch/stocks.csv

--- fetch/api.py
+++ fetch/api.py
@@ -6,7 +6,7 @@ finnhub_client = finnhub.Client(api_key=os.environ['API_KEY'])
 
 def fetch_stocks() -> pd.DataFrame:
 
-    df = pd.read_csv("stocks.csv")
+    df = pd.read_csv("fetch/stocks.csv")
 
     return pd.DataFrame([finnhub_client.quote(symbol) for symbol in df['symbol']], index=df['symbol'])
 
